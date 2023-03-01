a170a4b4fa90ea52051b2e0f866d808fb85a5958
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-01 01:24:09 -0300

    updated colors default theme scheme, updated font color in global styles

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle, css } from 'styled-components';
 
 export default createGlobalStyle`
-  ${({ theme: { colors: { background } } }) => css`
+  ${({ theme: { colors: { background, gray } } }) => css`
     * {
       margin: 0;
       padding: 0;
@@ -12,6 +12,7 @@ export default createGlobalStyle`
     body {
       background: ${background};
       font-size: 16px;
+      color: ${gray[900]}
     }
 
     button {
