c6de720bf7e3ebb6ffc845e964ade112da2952a6
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-01 13:45:10 -0300

    added PageHeader component

---
+++ src/components/PageHeader/styles.js
@@ -0,0 +1,30 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.header`
+  ${({ theme: { colors: { primary } } }) => css`
+    a {
+      text-decoration: none;
+      display: flex;
+      align-items: center;
+      transition: all 0.2s ease-in;
+
+      span {
+        font-weight: bold;
+        color: ${primary.main};
+      }
+
+      img {
+        transform: rotate(-90deg);
+        margin-right: 8px;
+      }
+
+      &:hover {
+        opacity: 0.80;
+      }
+    }
+
+    h1 {
+      font-size: 24px;
+    }
+  `}
+`;
