3e34a7baddc1ce4ad5213fa21238c9a360bd234b
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-02 12:39:56 -0300

    created util function isEmailValid and added validation to form fields (name, email)

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,31 +1,99 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 import FormGroup from '../FormGroup';
-import { Form, ButtonContainer } from './styles';
+import isEmailValid from '../../utils/isEmailValid';
+import { ButtonContainer } from './styles';
+
+const NAME_FIELD = 'name';
+const EMAIL_FIELD = 'email';
+const PHONE_FIELD = 'phone';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    const fieldName = event.target.name;
+    const message = 'Nome é obrigatório.';
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: fieldName, message },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== fieldName));
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    const fieldName = event.target.name;
+    const message = 'E-mail inválido.';
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const errorAlreadyExists = errors.find((error) => error.field === fieldName);
+
+      if (errorAlreadyExists) {
+        return;
+      }
+
+      setErrors((prevState) => [
+        ...prevState,
+        { field: fieldName, message },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== fieldName));
+    }
+  }
+
+  function getErrorMessageByFieldName(fieldName) {
+    return errors.find(({ field }) => field === fieldName)?.message;
+  }
+
+  function handlePhoneChange(e) {
+    setPhone(e.target.value);
+  }
+
+  function handleCategoryChange(e) {
+    setCategory(e.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  const nameError = getErrorMessageByFieldName(NAME_FIELD);
+  const emailError = getErrorMessageByFieldName(EMAIL_FIELD);
+
   return (
-    <Form>
-      <FormGroup>
-        <Input type="text" placeholder="Nome" />
+    <form onSubmit={handleSubmit}>
+      <FormGroup error={nameError}>
+        <Input name={NAME_FIELD} type="text" placeholder="Nome" value={name} onChange={handleNameChange} error={nameError} />
+      </FormGroup>
+
+      <FormGroup error={emailError}>
+        <Input name={EMAIL_FIELD} type="text" placeholder="E-mail" value={email} onChange={handleEmailChange} error={emailError} />
       </FormGroup>
 
       <FormGroup>
-        <Input type="text" placeholder="E-mail" />
+        <Input name={PHONE_FIELD} type="text" placeholder="Telefone" value={phone} onChange={handlePhoneChange} />
       </FormGroup>
 
       <FormGroup>
-        <Input type="text" placeholder="Telefone" />
-      </FormGroup>
-
-      <FormGroup>
-        <Select>
+        <Select value={category} onChange={handleCategoryChange}>
+          <option value="">Categoria</option>
           <option value="1">Instagram</option>
-          <option value="2">Facebook</option>
-          <option value="3">Trabalho</option>
-          <option value="4">Curso JStack</option>
+          <option value="2">Discord</option>
         </Select>
       </FormGroup>
 
@@ -34,7 +102,7 @@ export default function ContactForm({ buttonLabel }) {
           {buttonLabel}
         </Button>
       </ButtonContainer>
-    </Form>
+    </form>
   );
 }
 
