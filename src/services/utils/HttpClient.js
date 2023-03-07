03907c18fc320234031923f736eddf9279d3c8c0
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-07 11:59:12 -0300

    throwing error on httpClient when api returns error

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -8,7 +8,12 @@ class HttpClient {
   async get(path) {
     const response = await fetch(`${this.baseUrl}${path}`);
     await delay(500);
-    return response.json();
+
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error(`${response.status} - ${response.statusText}`);
   }
 }
 
