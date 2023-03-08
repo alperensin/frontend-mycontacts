d0aa63d101a897b7434c1092cf83d59523c5bc2b
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-08 13:07:32 -0300

    showing SearchNotFoundContainer container when search user

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,14 @@ import {
 } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Container, InputSearchContainer, Header, ListHeader, Card, ErrorContainer, EmptyListContainer,
+  Container,
+  InputSearchContainer,
+  Header,
+  ListHeader,
+  Card,
+  ErrorContainer,
+  EmptyListContainer,
+  SearchNotFoundContainer,
 } from './styles';
 import formatPhone from '../../utils/formatPhone';
 import Loader from '../../components/Loader';
@@ -16,6 +23,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import ContactsService from '../../services/ContactsService';
 
@@ -117,6 +125,17 @@ export default function Home() {
                   para cadastrar o seu primeiro!
                 </p>
               </EmptyListContainer>
+            )
+          }
+
+          {
+            (contacts.length > 0 && filteredContacts.length < 1) && (
+              <SearchNotFoundContainer>
+                <img src={magnifierQuestion} alt="Magnifier Question" />
+                <span>
+                  Nenhum resultado foi encontrado para <strong>”{searchTerm}”</strong>.
+                </span>
+              </SearchNotFoundContainer>
             )
           }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -180,3 +180,17 @@ export const EmptyListContainer = styled.div`
     }
   `}
 `;
+
+export const SearchNotFoundContainer = styled.div`
+  ${({ theme: { colors: { gray } } }) => css`
+    margin-top: 16px;
+    display: flex;
+    align-items: flex-start;
+
+    span {
+      color: ${gray[200]};
+      margin-left: 24px;
+      word-break: break-word;
+    }
+  `}
+`;
