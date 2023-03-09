6a431baf97d68ea293f9f85ee515e1f734647e16
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-09 13:54:47 -0300

    created CustomEvent to handle toast events

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react';
 import ToastMessage from '../ToastMessage';
 import { Container } from './styles';
 
 export default function ToastContainer() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    function handleAddToast(event) {
+      const { type, text } = event.detail;
+      setMessages((prevState) => [...prevState, { id: Math.random(), type, text }]);
+    }
+
+    document.addEventListener('addtoast', handleAddToast);
+
+    return () => {
+      document.removeEventListener('addtoast', handleAddToast);
+    };
+  }, []);
+
   return (
     <Container>
-      <ToastMessage text="Default Toast" />
-      <ToastMessage text="Error Toast" type="danger" />
-      <ToastMessage text="Success Toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage key={message.id} type={message.type} text={message.text} />
+      ))}
     </Container>
   );
 }
