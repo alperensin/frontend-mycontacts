e5f3853e81ca7774aaae430ba8a88c9bc3bbefe1
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-10 03:22:13 -0300

    loading and showing contact data on form

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,11 +1,42 @@
+import { useEffect, useRef, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
 import PageHeader from '../../components/PageHeader';
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+        contactFormRef.current.setFieldsValues(contact);
+        setIsLoading(false);
+      } catch (error) {
+        history.push('/');
+        toast({ type: 'danger', text: 'Contato não encontrado!' });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  async function handleSubmit() {
+    // OnSubmit form
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader title="Editar André Luiz" />
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm ref={contactFormRef} buttonLabel="Salvar alterações" onSubmit={handleSubmit} />
     </>
   );
 }
