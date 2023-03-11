4c591ba338f9172908a06f73d1a5adfb1943454a
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-11 16:07:44 -0300

    refactoring the new contact screen, moving the logic to a custom hook

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,21 +1,9 @@
-import { useRef } from 'react';
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
-import ContactsService from '../../services/ContactsService';
-import toast from '../../utils/toast';
+import useNewContact from './useNewContact';
 
 export default function NewContact() {
-  const contactFormRef = useRef(null);
-
-  async function handleSubmit(contact) {
-    try {
-      await ContactsService.createContact(contact);
-      contactFormRef.current.resetFields();
-      toast({ type: 'success', text: 'Contato cadastrado com sucesso!' });
-    } catch (error) {
-      toast({ type: 'danger', text: `Ocorreu um erro ao cadastrar contato: ${error.message}.` });
-    }
-  }
+  const { contactFormRef, handleSubmit } = useNewContact();
 
   return (
     <>
