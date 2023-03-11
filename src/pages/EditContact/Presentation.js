ed659df03f4b8081cd1316344a86c5cc07450078
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-11 14:49:21 -0300

    refactoring the contact editing screen using the container/presentational pattern

---
+++ src/pages/EditContact/Presentation.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+import PageHeader from '../../components/PageHeader';
+
+export default function Presentation({
+  isLoading, contactName, contactFormRef, onSubmit,
+}) {
+  return (
+    <>
+      <Loader isLoading={isLoading} />
+      <PageHeader title={isLoading ? 'Carregando...' : `Editar ${contactName}`} />
+      <ContactForm ref={contactFormRef} buttonLabel="Salvar alterações" onSubmit={onSubmit} />
+    </>
+  );
+}
+
+Presentation.propTypes = {
+  isLoading: PropTypes.bool,
+  contactName: PropTypes.string.isRequired,
+  contactFormRef: PropTypes.shape().isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+Presentation.defaultProps = {
+  isLoading: true,
+};
