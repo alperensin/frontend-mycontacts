7f144d275519a9832c34bb1f604a6dc127edc41c
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-13 10:18:29 -0300

    refactoring the Modal component to use the custom useAnimatedUnmount hook

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,36 +1,13 @@
-import { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Overlay, Container, Footer } from './styles';
 import Button from '../Button';
 import ReactPortal from '../ReactPortal';
+import useAnimatedUnmount from '../../hooks/useAnimatedUnmount';
 
 export default function Modal({
   danger, title, children, cancelLabel, confirmLabel, onCancel, onConfirm, visible, isLoading,
 }) {
-  const [shouldRender, setShouldRender] = useState(visible);
-  const overlayRef = useRef(null);
-
-  useEffect(() => {
-    if (visible) {
-      setShouldRender(true);
-    }
-
-    function handleAnimationEnd() {
-      setShouldRender(false);
-    }
-
-    const overlayRefElement = overlayRef.current;
-
-    if (!visible && overlayRefElement) {
-      overlayRefElement.addEventListener('animationend', handleAnimationEnd);
-    }
-
-    return () => {
-      if (overlayRefElement) {
-        overlayRefElement.removeEventListener('animationend', handleAnimationEnd);
-      }
-    };
-  }, [visible]);
+  const { shouldRender, animatedElementRef } = useAnimatedUnmount(visible);
 
   if (!shouldRender) {
     return null;
@@ -38,7 +15,7 @@ export default function Modal({
 
   return (
     <ReactPortal containerId="modal-root">
-      <Overlay ref={overlayRef} isLeaving={!visible}>
+      <Overlay ref={animatedElementRef} isLeaving={!visible}>
         <Container isLeaving={!visible} danger={danger}>
           <h1>{title}</h1>
           <div className="modal-body">
