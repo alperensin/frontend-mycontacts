91011dd2cf9d607c56ca94783101383bcf723ef5
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-14 12:14:40 -0300

    Using the Routes/useRoutes component in place of the switch

---
+++ src/Router.js
@@ -0,0 +1,35 @@
+import {
+  // Route,
+  // Routes,
+  useRoutes,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import NewContact from './pages/NewContact';
+import { Container as EditContact } from './pages/EditContact';
+
+export default function Router() {
+  const routes = useRoutes([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/new',
+      element: <NewContact />,
+    },
+    {
+      path: '/edit/:id',
+      element: <EditContact />,
+    },
+  ]);
+
+  return routes;
+
+  // return (
+  //   <Routes>
+  //     <Route path="/" element={<Home />} />
+  //     <Route path="/new" element={<NewContact />} />
+  //     <Route path="/edit/:id" element={<EditContact />} />
+  //   </Routes>
+  // );
+}
